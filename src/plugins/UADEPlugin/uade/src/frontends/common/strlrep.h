75b2e262636c5bbbfb84a3e4230800675c8d4a39
Jonas Minnberg <sasq64@gmail.com>
2014-03-06 22:05:06 +0100

    missed 2 files

---
+++ src/plugins/UADEPlugin/uade/src/frontends/common/strlrep.h
@@ -0,0 +1,10 @@
+#ifndef _UADE_STRLREP_H_
+#define _UADE_STRLREP_H_
+
+#include <string.h>
+
+size_t strlcpy(char *dst, const char *src, size_t size);
+size_t strlcat(char *dst, const char *src, size_t size);
+
+#endif
+
