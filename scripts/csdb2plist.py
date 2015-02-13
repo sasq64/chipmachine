2b169b58ece242bd8a4f214b4b3376939f37aca2
Jonas Minnbergh <jonasm@jonasmi-macbook.local>
2015-02-13 20:50:36 +0100

    Added scripts

---
+++ scripts/csdb2plist.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+import os
+import sys
+from xml.dom.minidom import Document, parseString
+	
+
+def getText(node, tag) :
+	nextNode = None
+	tags = tag.split('/')
+	for t in tags :
+		#print t, node
+		for c in node.childNodes :
+			# print ">> ", c.nodeType
+			if c.nodeType == c.ELEMENT_NODE :
+				# print ">> ", c.tagName
+				if c.tagName == t :
+					nextNode = c
+					break
+		if nextNode == None :
+			return ''
+		node = nextNode
+		nextNode = None
+						
+	rc = ''
+	# print "Found node ", node
+	for t in node.childNodes :		
+		if t.nodeType == c.TEXT_NODE :
+			rc += t.data
+	return rc
+
+
+
+def main(argv) :
+	try :
+		xmlData = open(argv[0]).read()
+	except :
+		xmlData = None
+		
+	#outf = open(argv[1], 'w')
+		
+	groupList = {}
+	eventList = {}
+	
+	code = 'iso8859_1'
+		
+	if xmlData :
+		doc = parseString(xmlData)
+		xmlreleases = doc.getElementsByTagName('Release')
+		#print "Found %d releases" % (len(releases))
+ 		#outf.write("[Releases]\n")
+		#try :
+
+		events = {}
+		releases = {}
+
+		for r in xmlreleases :
+			rel = {}
+		
+			name = getText(r, 'Name').strip()
+			id = getText(r, 'ID')
+			if id :
+				id = int(id)
+			else :
+				id = -1
+			rating = getText(r, 'CSDbRating')
+			rt = 0
+			if rating :
+				rt = int(float(rating) * 100)
+				rating = str(rt)
+		
+			#if rt < 900 :
+			#	continue
+		
+			group = getText(r, 'ReleasedBy/Group/Group').strip()
+			event = getText(r, 'Achievement/Event/Name').strip()
+			compo = getText(r, 'Achievement/Compo').strip()
+			place = getText(r, 'Achievement/Place').strip()
+			if place :
+				place = int(place)
+			else :
+				place = 0
+
+			type = getText(r, 'ReleaseType')
+
+			if type == 'C64 Music' and event :
+				if event in events :
+					events[event].append((place, id))
+				else :
+					events[event] = [ (place, id) ]
+
+			sids = r.getElementsByTagName('HVSCPath')
+			fnames = []
+			for s in sids :			
+				fnames.append(s.firstChild.data)
+
+			releases[id] = (name, group, fnames)
+		
+			if (type.endswith('Music Collection') or type.endswith('Demo')) and rt > 0 :
+				print(type, name)
+				sids = r.getElementsByTagName('HVSCPath')
+				fnames = []
+				for s in sids :			
+					fnames.append(s.firstChild.data)
+				outf = open('r%d.plist' % (id,), 'w')
+				outf.write((u';%s\t%s\n' % (name, group)).encode(code))
+				for fn in fnames :
+					if fn[0] == '/' :
+						fn = fn[1:]
+					outf.write('csdb::%s\n' % (fn,))
+				outf.close()
+		i = 0				
+		for ename in events :
+			print(ename)
+			e = events[ename]
+			e.sort()
+			outf = open('p%d.plist' % (i,), 'w')
+			i+=1
+			outf.write((u';%s Music Compo\n' % (ename,)).encode(code))
+			for x in e :
+				rel = releases[x[1]]
+				for fn in rel[2] :
+					if fn[0] == '/' :
+						fn = fn[1:]
+					outf.write('csdb::%s\n' % (fn,))
+			outf.close()
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
---
+++ scripts/songlen.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+
+import os
+import sys
+import struct
+from bisect import bisect
+
+def parse(fname) :
+    f = open(fname)
+    
+    hashMap = {}
+    lastName = ""
+    
+    for line in f :
+        a = line.split('=')
+        if len(a) == 2 :
+            hash = int(a[0][:8], 16)
+            
+            if lastName.find('Savage.sid') > -1 :
+                print "%s %s => %08x" % (lastName, a[0], hash)
+            
+            if hashMap.has_key(hash) :
+                print "Found duplicate ", lastName
+            
+            lengths = []
+            lens = a[1].split(' ')
+            for l in lens :
+                tt = l.split(':')
+                if len(tt) == 2 :
+                    #print tt
+                    sec = int(tt[0]) * 60 + int(tt[1][:2])
+                    lengths.append(sec)
+                    
+                    
+            hashMap[hash] = lengths
+
+        else :
+            lastName = line
+    return hashMap
+                
+
+    
+def main(argv) :
+    hashmap = parse(argv[0])
+    
+    f = open('out.dat', 'w')
+    
+    
+    a = sorted(hashmap)
+    f.write(struct.pack('>I', len(a)))
+    offset = 0
+    offsets = []
+
+    for x in a :
+        lens = hashmap[x]
+        
+        if len(lens) == 1 :        
+            f.write(struct.pack('>IH', x, lens[0]))
+        else :
+            f.write(struct.pack('>IH', x, offset | 0x8000))
+            for i in range(len(lens)-1) :
+                l = lens[i]
+                offsets.append(struct.pack('>H', l))            
+            offsets.append(struct.pack('>H', lens[-1] | 0x8000))
+            offset += len(lens)
+    
+    print "Offset is ", offset
+    f.write(''.join(offsets))
+    
+    
+import hashlib
+
+table = None
+tablen = -1
+tabdata = None
+
+
+def lookup(f):
+    global table, tablen, tabdata
+    m = hashlib.md5()
+    data = open(f).read()
+    
+    offset = 120
+    a = struct.unpack('>4sHHHHHHHI32s32s32s', data[0:118])
+    if a[1] == 2 :
+        b = struct.unpack('>HBBH', data[118:124])
+        offset = 126
+        
+    # a[6] = songs, a[8] = speed
+    
+    print "%d songs, %x speed, %x flags" % (a[6], a[8], b[0])
+    
+    m.update(data[offset:])
+    
+    print "LEN ", len(data[offset:])
+    
+    m.update(data[0xb:0xc])
+    m.update(data[0xa:0xb])
+    m.update(data[0xd:0xe])
+    m.update(data[0xc:0xd])
+    m.update(data[0xf:0x10])
+    m.update(data[0xe:0xf])
+    
+    temp = '\x3c'
+    
+    for i in range(a[6]) :
+        print ord(temp[0])
+        m.update(temp)
+   # m.update(temp)
+
+    #m.update(data)
+    md5 = m.hexdigest()    
+    hash = int(md5[:8], 16)
+    
+    print "%s => %s => %08x" % (f, md5, hash)
+    
+    if not table :
+        tabdata = open('out.dat').read()
+        tablen = struct.unpack('>L', tabdata[0:4])[0]
+        table = []
+        for i in range(tablen) :
+            j = i*6 + 4
+            x = struct.unpack('>LH', tabdata[j:j+6])
+            table.append(x[0])
+        
+    where = bisect(table, hash)    
+    y = struct.unpack('>LH', tabdata[where*6+4:where*6+10])
+    print "%d -> Hash %02x -> %02x (%02d)" % (where, hash, y[0], y[1])
+    where -= 1
+    x = struct.unpack('>LH', tabdata[where*6+4:where*6+10])        
+    print "%d -> Hash %02x -> %02x (%02d)" % (where, hash, x[0], x[1] & 0x7fff)
+    
+    if x[1] & 0x8000 :
+        offset = 4 + tablen * 6 + (x[1] & 0x7fff) * 2
+        print struct.unpack('>HHHH', tabdata[offset:offset+8])
+    
+    
+    
+        
+
+
+def test(f) :
+    lookup(f, False, 0)
+    lookup(f, True, 0)
+    lookup(f, False, 2)
+    lookup(f, True, 2)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
