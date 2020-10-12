a96ee0b41a3a5e553ddef838ab66cabb536e1e0f
Michael Straube <michael.straubej@gmail.com>
2020-10-12 13:13:28 +0200

    Fix build with gcc10

--- src/di.hpp
+++ src/di.hpp
@@ -70,7 +70,7 @@ BOOST_DI_CFG_FWD
 #        pragma clang diagnostic error "-Wundefined-internal"
 #        pragma clang diagnostic ignored "-Wmissing-field-initializers"
 #    elif defined(__GCC__)
-#        pragma GCC diagnostic error "-Wdeprecated-declarations"
+#        pragma GCC diagnostic warning "-Wdeprecated-declarations"
 #        pragma GCC diagnostic push
 #        if (__GNUC__ < 6)
 #            pragma GCC diagnostic error "-Werror"
